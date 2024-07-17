0792aa769c781e4905438711dbac7ea1eeb0c35f
Akash Parmar <ak.parmar1442000@gmail.com>
2024-07-17 12:57:43 +0530

    components of template

---
+++ template/src/components/Experience.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Experience = () => {
+  return (
+    <>
+      <directionalLight position={[1,2,3]} intensity={1.5}/>
+      <ambientLight intensity={1.5}/>
+      <group >
+        <mesh scale={1.5} position-x={2} rotation-y={Math.PI * 0.25}>
+          <boxGeometry />
+          <meshStandardMaterial color="mediumpurple" />
+        </mesh>
+
+        <mesh  position-x={-2}>
+          <sphereGeometry />
+          <meshStandardMaterial color="orange" />
+        </mesh>
+      </group>
+
+      <mesh rotation-x={-Math.PI * 0.5} scale={20} position-y={-3}>
+        <planeGeometry />
+        <meshStandardMaterial color="greenyellow" />
+      </mesh>
+    </>
+  );
+};
+
+export default Experience;
