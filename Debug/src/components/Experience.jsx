5ad2100b116a22797ee9e0228bd2f74cd4ad6009
Akash Parmar <ak.parmar1442000@gmail.com>
2024-07-17 13:44:24 +0530

    removed group tag from template and debug

--- Debug/src/components/Experience.jsx
+++ Debug/src/components/Experience.jsx
@@ -6,7 +6,6 @@ const Experience = () => {
       <OrbitControls />
       <directionalLight position={[1,2,3]} intensity={1.5}/>
       <ambientLight intensity={1.5}/>
-      <group >
         <mesh scale={1.5} position-x={2} rotation-y={Math.PI * 0.25}>
           <boxGeometry />
           <meshStandardMaterial color="mediumpurple" />
@@ -16,7 +15,6 @@ const Experience = () => {
           <sphereGeometry />
           <meshStandardMaterial color="orange" />
         </mesh>
-      </group>
 
       <mesh rotation-x={-Math.PI * 0.5} scale={20} position-y={-3}>
         <planeGeometry />
